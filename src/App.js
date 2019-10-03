71e45e90bacc7c95af4794e99881984135c0d628
“Erin <erinkelley27@gmail.com>
2019-10-02 20:41:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div class="App">
+        <h1>Erin Kelley</h1>
+        <h3>Software Engineer</h3>
+        <div class="links">
+          <a href="https://drive.google.com/file/d/0B11GLK3AEj9bS3FsZHNObWpwdFY2U3Jxd2F4YTEzUFhqcEpB/view?usp=sharing" target="_blank">Resume</a>
+          <a href="https://www.linkedin.com/in/erinmargaretkelley/" target="_blank">LinkedIn</a>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
